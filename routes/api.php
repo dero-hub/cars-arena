346be1faf8ba5d56c8aa63b61d0a9feb03be61aa
dero-hub <munenederrick862@gmail.com>
2022-07-25 22:53:53 +0300

    Wrote logic to handle Roles model create read and delete

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
 use App\Http\Controllers\Users_controller;
+use App\Http\Controllers\Roles_controller;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +33,15 @@ Route::group([
     Route::put('/user/update', [Users_controller::class, 'update']);
     Route::get('/user/list', [Users_controller::class, 'findAll']);
     Route::delete('/user/delete/{id}', [Users_controller::class, 'deleteOne']);
-    Route::delete('/user/delete', [Users_controller::class, 'deleteAll']);
-        
+    Route::delete('/user/delete', [Users_controller::class, 'deleteAll']);     
+});
+
+Route::group([
+    'middleware' => 'api',
+], function ($router) {
+    Route::post('/roles/create', [Roles_controller::class, 'create']);
+    Route::get('/roles/list/{id}', [Roles_controller::class, 'findOne']);
+    Route::get('/roles/list', [Roles_controller::class, 'findAll']);
+    Route::delete('/roles/delete/{id}', [Roles_controller::class, 'deleteOne']);
+    Route::delete('/roles/delete', [Roles_controller::class, 'deleteAll']);     
 });
